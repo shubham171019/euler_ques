12a70aeb0fcb7e5750491e804fdb94478d5bbd75
shubham171019 <41218386+shubham171019@users.noreply.github.com>
2018-09-05 17:38:56 +0530

    Update ques6.java

--- ques6.java
+++ ques6.java
@@ -3,29 +3,15 @@ public class ques6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double sum=0  ;
+	
+		int x=0,y=0;
 		for(int i=1;i<=100;i++)
 		{
-			sum = sum + Math.pow(i,2);
-			
+			x+=i;
+			y+=(i*i);
 		}
-		System.out.println(sum);
-		long sum2=0;
-		int sum1 =0;
-		for(int j=1;j<=100;j++)
-		{
-			sum1= sum1 + j;
-			
-		}
-		
-		 sum2 = (long)sum1 * (long)sum1;
-		
-		System.out.println(sum1);
-		System.out.println(sum2);
-		
-		int dif;
-		dif = (int)sum2- (int)sum;
-		System.out.println(dif);
+		x*=y;
+		System.out.println(x-y);
 	}
 
 }
