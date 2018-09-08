cc86b032d79592f21080aafce98d056c41d1eb7b
shubham171019 <41218386+shubham171019@users.noreply.github.com>
2018-09-09 00:23:35 +0530

    Update ques5.java

--- ques5.java
+++ ques5.java
@@ -3,28 +3,29 @@ public class ques5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		long c = 21;
-		while (1==1)
+		int lcm=11;
+		int x,y;
+		
+		for(int i=1;i<20;i++)
 		{
-		    boolean all = true;
-		    for (int i = 1; i < 21; i++)
-		    {
-		        all = true;
-		        if ( c % i != 0 )
-		        {
-		            all = false;
-		            break;
-		        }
-		    }
-		    if ( all )
-		    {
-		        System.out.println( c );
-		        System.exit(0);
-		    }
-
-		    c++;
+			x=lcm;
+			y=i;
+			while(x!=y)
+			{
+				if(x>y)
+				{
+					x=x-y;
+				}
+					
+				else
+				{
+					y=y-x;
+				}
+					
+			}
+			lcm=(lcm*i)/x;
 		}
-
+		System.out.println("LCM= "+lcm);
 	}
 
 }
